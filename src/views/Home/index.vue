8f045d89364fde3178136a639b894260d56a1e1d
0546zhangyifan <zyf1536678868@gmail.com>
2020-04-28 17:58:28 +0800

    轮播图

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,23 +1,54 @@
 <template>
   <div id="app">
     <PublicHeader :current=0></PublicHeader>
+    <section class="main">
+      <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide" v-for="item in bannerList" :key="item.id">
+              <img :src="item.pic_url" alt="">
+            </div>
+        </div>
+        <div class="swiper-pagination"></div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 import PublicHeader from '@/views/Header'
+import Swiper from 'swiper'
+import 'swiper/css/swiper.css'
 
 export default {
   name: 'Home',
   components: {
     PublicHeader
   },
+  data () {
+    return {
+      bannerList: []
+    }
+  },
+  mounted () {
+    /* eslint-disable */
+    setTimeout(() => {
+      new Swiper('.swiper-container', {
+        pagination: {
+          el: '.swiper-pagination',
+          clickable :true,
+        },
+        loop : true,
+      })
+    }, 200);
+    /* eslint-disable */
+  },
   created () {
     this.$axios({
-      url: 'http://localhost:8080/buka/api/v3/home?page=1',
+      url: 'buka/api/v3/home?page=1',
       method: 'GET'
     }).then(res => {
-      console.log(res.data)
+      this.bannerList = res.data.datas.items[0].items
+      console.log(res.data.datas.items[0].items.length)
     }).catch(err => {
       console.log(err)
     })
@@ -30,5 +61,24 @@ export default {
   height: 100%;
   display: flex;
   flex-direction: column;
+  .main{
+    height: 100%;
+    width: 100%;
+    flex: 1;
+    margin-top: 89px;
+    padding: 7.5px;
+    flex-direction: column;
+    .swiper-container{
+      height: 240px;
+      width: 100%;
+      .swiper-wrapper{
+        .swiper-slide{
+          img{
+            width: 100%;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
